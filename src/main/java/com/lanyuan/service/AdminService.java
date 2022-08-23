1af4b00ec6b050f29e497d38496844ba2ef5aaa9
hjm <wy17396433052@163.com>
2022-08-23 15:59:09 +0800

    fifth Commit

---
+++ src/main/java/com/lanyuan/service/AdminService.java
@@ -0,0 +1,25 @@
+package com.lanyuan.service;
+
+import com.github.pagehelper.PageInfo;
+import com.lanyuan.pojo.Admin;
+
+public interface AdminService {
+
+    Admin login(Admin a);
+
+    PageInfo<Admin> show(Integer pageNum, Integer pageSize);
+
+    int add(Admin a);
+
+    int delete(Integer id);
+
+    Admin selectById(Integer id);
+
+    int update(Admin admin);
+
+
+    //下面这两个方法是利用shiro而增加的两个方法
+    Admin selectByUsernames(String username);
+
+
+}
---
+++ src/main/java/com/lanyuan/service/impl/AdminServiceImpl.java
@@ -0,0 +1,60 @@
+package com.lanyuan.service.impl;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.lanyuan.mapper.AdminMapper;
+import com.lanyuan.pojo.Admin;
+import com.lanyuan.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class AdminServiceImpl implements AdminService {
+
+    @Autowired
+    AdminMapper mapper;
+
+    @Override
+    public Admin login(Admin a) {
+        return mapper.login(a);
+    }
+
+    @Override
+    public PageInfo<Admin> show(Integer pageNum, Integer pageSize) {
+        PageHelper.startPage(pageNum,pageSize);
+        List<Admin> list = mapper.show();
+        return new PageInfo<>(list);
+    }
+
+    @Override
+    public int add(Admin a) {
+        return mapper.insertSelective(a);
+    }
+
+    @Override
+    public int delete(Integer id) {
+        return mapper.deleteByPrimaryKey(id);
+    }
+
+    @Override
+    public Admin selectById(Integer id) {
+        return mapper.selectByPrimaryKey(id);
+    }
+
+    @Override
+    public int update(Admin admin) {
+        //动态更新
+        return mapper.updateByPrimaryKeySelective(admin);
+    }
+
+    @Override
+    public Admin selectByUsernames(String username) {
+        //return mapper.selectByUsernames(username);
+        return null;
+    }
+
+}
