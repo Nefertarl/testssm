26812c1a1be17d5d55a455e8a6ccb0fbb8c08b44
Nefertarl <99526922+Nefertarl@users.noreply.github.com>
2022-08-23 13:17:05 +0800

    Delete RelationMapper.java

--- src/main/java/com/lanyuan/mapper/RelationMapper.java
+++
@@ -1,9 +0,0 @@
-package com.lanyuan.mapper;
-
-import com.lanyuan.pojo.Relation;
-
-public interface RelationMapper {
-    int insert(Relation record);
-
-    int insertSelective(Relation record);
-}
